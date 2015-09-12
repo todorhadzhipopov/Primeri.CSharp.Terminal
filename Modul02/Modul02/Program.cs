108c30c8eaed1457d267d2cce59e185533f0e1a0
Todor Hadzhipopov <todorgh@abv.bg>
2015-09-12 19:21:11 +0300

    Модук 02, /видео 02 - променливи от тип int

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -8,12 +8,21 @@ namespace Modul02
 		{
 
 			//Дефиниране на променливи
+			int a = 0, b = 0;
+			int sum = 0;
 
 			//Събриране на числа
-
+			a = 55;
+			b = 10;
+			sum = a + b;
 			//Писане в конзола
-
+			Console.Write ( a );
+			Console.Write (" + ");
+			Console.Write ( b );
+			Console.Write (" = ");
+			Console.WriteLine ( sum );
 		
+
 		}
 	}
 }
