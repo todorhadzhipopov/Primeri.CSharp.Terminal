f9790d9c01dfec159c0edfa134487dbbcd6d91ad
Todor Hadzhipopov <todorgh@abv.bg>
2015-09-27 20:59:55 +0300

    Модул 03, Видео 05 - Примери с тип bool

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -16,11 +16,17 @@ namespace BoolExplore
 			bool _check01= _ui <= _i.Length;
 			bool _check02 = _ui > 0;
 
+			bool _exp01 = _check01 && _check02; //Логическo и "&&"
+			bool _exp02 = _check01 || _check02; //Логическо или "||"
+			bool _exp03 = ! _check01;  //Отрицание "!"
 
 
-			Console.WriteLine (_check01);
-			Console.WriteLine (_check02);
-			Console.WriteLine ( _i [ _ui - 1]);
+			Console.WriteLine ("Логическо И: " + _exp01);
+			Console.WriteLine ("Логическо Или: "+ _exp02);
+			Console.WriteLine ("Отрицание на : " + _check01 + ": " + _exp03);
+
+
+			//Console.WriteLine ( _i [ _ui - 1]);
 
 		}
 	}
