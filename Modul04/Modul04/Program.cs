9f219d32dd99382eb4c6371076c16aff0dd7c8f3
Todor Hadzhipopov <todorgh@abv.bg>
2015-10-04 21:27:46 +0300

    Модул 04, Видео 01 - Цикъл for

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			for (int i = 0; i <=5; i+=2) 
+			{ 
+				//Код на цикъла
+				Console.WriteLine (i.ToString ());
+			}
+
+		}
+	}
+}
