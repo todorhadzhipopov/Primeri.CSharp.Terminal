aef86bde1401fecc3fe5239d7982255677316129
Todor Hadzhipopov <todorgh@abv.bg>
2015-10-07 21:17:33 +0300

    Модул 04, Видео 06 - Примери с цикъл do ..while

---
+++ Modul04/ListExplore/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ListExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниции
+			string _userinput = "";
+
+
+			//Потребителски вход
+			do {
+				
+				Console.Write ("? ");
+				_userinput = Console.ReadLine ();
+
+
+				//Добавяне на стойности
+
+
+				//Преглед на List
+
+				//Размер на List
+
+			}		
+			while (_userinput !="exit");
+		
+		
+		
+		}
+	}
+}
