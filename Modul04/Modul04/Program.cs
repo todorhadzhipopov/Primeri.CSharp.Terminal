3708131516b3bb4d62920163c1d0f3ea1892a99f
Todor Hadzhipopov <todorgh@abv.bg>
2015-10-07 20:54:41 +0300

    Модул 04, Видео 04 - Цикъл while

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -21,13 +21,26 @@ namespace Modul04
 		
 
 			//Тяло на таблица
-			for (int i = 0; i < _table.Length / 3; i++) 
-			{ 
-				for (int j = 0; j < 3; j++)
-				{
-				Console.Write (_table [i, j].PadRight (12));
-				}
-				Console.WriteLine ();
+
+			//Примери с for
+//			for (int i = 0; i < _table.Length / 3; i++) 
+//			{ 
+//				for (int j = 0; j < 3; j++)
+//				{
+//				Console.Write (_table [i, j].PadRight (12));
+//				}
+//				Console.WriteLine ();
+//			}
+
+			//Примери с while
+			int i =0;
+
+			while (i < _table.Length / 3 )
+			{
+				Console.WriteLine (_table[i,0].PadRight(12) +
+					_table[i,1].PadRight (12) +
+					_table[i,2].PadRight (12));
+				i++;
 			}
 
 		}
